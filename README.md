0924fea0ee56773809efb56590afd9f9abb15671
Tony Ko <htko89@gmail.com>
2017-02-03 14:47:18 -0500

    Update: First complete working sample

--- README.md
+++ README.md
@@ -24,3 +24,7 @@ Build an app that is functionally similar to this: https://codepen.io/FreeCodeCa
 * User Story: I can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.
 * User Story: I can reset the clock for my next pomodoro.
 * User Story: I can customize the length of each pomodoro.
+
+## Attribution
+Tomato icon made by [Freepik](http://www.freepik.com) from [www.flaticon.com](http://www.flaticon.com) licensed under [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/)
+Sounds from [Android Open Source Project](https://source.android.com/source/licenses.html) licensed under [Apache 2.0](http://www.apache.org/licenses/LICENSE-2.0)
--- release/static/js/index.js
+++ release/static/js/index.js
@@ -1,38 +1,96 @@
 $( document ).ready( function() {
 
-  var workTimer = new timer();
-  var btnState = "play";
+  // timer
 
-  $( "#btnPlay" ).click( function() {
-    var interval = $( "#inputWork" ).val();
-    console.log(btnState);
-    if ( btnState === "play" ) {
-      $( "#btnPlay i" ).removeClass( "fa-play" );
-      $( "#btnPlay i" ).addClass( "fa-pause" );
-      workTimer.start( interval, updateTimer, stopTimer );
-      btnState = "pause"
-    } else {
-      $( "#btnPlay i" ).removeClass( "fa-pause" );
-      $( "#btnPlay i" ).addClass( "fa-play" );
-      workTimer.stop( pauseTimer );
-      btnState = "play"
+  var pomoTimer = new timer();
+  var multiplier = 60; // set 1 for seconds, 60 for minutes. Used to speed up debugging.
+  var workTime = 0;
+  var playTime = 0;
+  var totalTime = 0;
+  var btnState = "play";
+  var timersElapsed = 0;
+  var workSound = new Audio( "static/audio/Salacia.ogg" );
+  var playSound = new Audio( "static/audio/Ariel.ogg" );
+
+  $( "#progress" ).circleProgress( {
+    size: 220,
+    thickness: 2,
+    startAngle: Math.PI / 4 * 6,
+    lineCap: "round",
+    value: 0,
+    animation: {
+      duration: 100
+    },
+    fill: {
+      gradient: [
+        [ "#f44336", .75 ],
+        [ "#b71c1c", .25 ]
+      ],
+      gradientAngle: Math.PI / 4 * 2
     }
   } );
 
-  $( "#btnStop" ).click( function() {
-    workTimer.stop( stopTimer );
+  $( "#btn-play" ).click( function() {
+    if ( btnState === "play" ) {
+      console.log( "btn play" );
+      $( "#btn-play i" ).attr( "class", "fa fa-stop" );
+      workTime = parseInt( $( "#input-work" ).val(), 10 );
+      playTime = parseInt( $( "#input-play" ).val(), 10 );
+      totalTime = workTime + playTime;
+      if ( workTime !== 0 ) {
+        pomoTimer.start( "work", workTime, inputUpdate, router );
+      } else {
+        if ( playTime !== 0 ) {
+          pomoTimer.start( "play", playTime, inputUpdate, router );
+        } else {
+          $( "#display .input" ).addClass( "animated shake" );
+        }
+      }
+      btnState = "stop";
+    } else {
+      console.log( "btn stop" );
+      pomoTimer.stop( "all", router );
+      btnState = "play";
+    }
   } );
 
-  function updateTimer( time ) {
-    $( "#inputWork" ).val( ( "0" + time ).slice( -2 ) );
+  function inputUpdate( name, time ) {
+    // console.log( "timer update", name, time );
+    var progTime = 0;
+    if ( name === "work" ) {
+      progTime = ( workTime - time ) / totalTime;
+    } else {
+      progTime = ( ( playTime - time ) + workTime ) / totalTime;
+    }
+    $( "#progress" ).circleProgress( "value", progTime );
+    $( "#input-" + name ).val( ( "0" + Math.round( time ) ).slice( -2 ) );
   }
 
-  function pauseTimer() {
-    // $( "#inputWork" ).val( "55" );
-  }
-
-  function stopTimer() {
-    $( "#inputWork" ).val( "00" );
+  function router( name, timer ) {
+    console.log( "timer stop", name )
+    if ( name === "work" ) {
+      $( "#input-work" ).val( "00" );
+      pomoTimer.start( "play", playTime, inputUpdate, router );
+    } else if ( name === "play" ) {
+      if (timersElapsed >= 12){
+        $( "#counter" ).html();
+        timersElapsed = 0;
+      }
+      $( "#input-work" ).val( ( "0" + workTime ).slice( -2 ) );
+      $( "#input-play" ).val( ( "0" + playTime ).slice( -2 ) );
+      $( "#progress" ).circleProgress( "value", 0 );
+      $( "#counter" ).append( "<img class=\"tomato\" src=\"static/img/tomato.png\">" );
+      pomoTimer.start( "work", workTime, inputUpdate, router );
+      timersElapsed++;
+    } else {
+      $( "#btn-play i" ).attr( "class", "fa fa-play" );
+      $( "#input-work" ).val( ( "0" + workTime ).slice( -2 ) );
+      $( "#input-play" ).val( ( "0" + playTime ).slice( -2 ) );
+      $( "#progress" ).circleProgress( "value", 0 );
+      $( "#counter" ).html();
+      btnState = "play";
+      timersElapsed = 0;
+    }
   }
 
   function timer() {
@@ -40,30 +98,33 @@ $( document ).ready( function() {
     var timeStart = 0;
     var started = false;
     var timer_id;
-    this.start = function( time, cbUpdateTimer, cbStopTimer ) {
+    this.start = function( name, time, cbUpdate, cbStop ) {
       if ( !started ) {
-
-        time *= 1000;
+        if ( name === "work" ) {
+          workSound.play();
+        } else {
+          playSound.play();
+        }
+        time *= 1000 * multiplier;
         timeStart = new Date().getTime();
         started = true;
         timer_id = setInterval( function() {
           var timeLeft = time - ( new Date().getTime() - timeStart );
           if ( timeLeft <= 0 ) {
-            stop( cbStopTimer );
+            started = false;
+            clearInterval( timer_id );
+            cbStop( name );
           } else {
-            cbUpdateTimer( Math.round( timeLeft / 1000 ) )
+            cbUpdate( name, timeLeft / 1000 / multiplier )
           };
-        }, 1000 ); // the smaller this number, the more accurate the timer will be}
-
+        }, 100 ); // the smaller this number, the more accurate the timer will be}
       }
     }
-    this.stop = function( cbStopTimer ) {
+    this.stop = function( name, cbStop ) {
       if ( started ) {
-
         started = false;
         clearInterval( timer_id );
-        cbStopTimer();
-
+        cbStop( name );
       }
     }
   }
